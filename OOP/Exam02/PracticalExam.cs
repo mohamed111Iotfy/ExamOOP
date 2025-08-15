4cc52429656df0055df1d05eb507a801f333ea08
mohamed lotfy <mohhamedlotfy@outlook.com>
2025-08-15 13:14:02 +0300

    Exam

---
+++ OOP/Exam02/PracticalExam.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Exam02
+{
+    internal class PracticalExam : BaseExam
+    {
+        public PracticalExam(TimeOnly time, int numberOfQuestions)
+            : base(time, numberOfQuestions)
+        {
+        }
+
+        public override void ShowExam()
+        {
+            foreach (var question in Questions)
+            {
+                question.Show();
+                Console.WriteLine("Your answer:");
+                int answer = int.Parse(Console.ReadLine()!);
+                Console.WriteLine($"correct Answer :{question.RightAnswers}");
+            }
+
+        }
+
+
+    }
+}
