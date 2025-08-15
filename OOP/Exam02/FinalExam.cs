24b8bccd83fd5ed88c40fe25b22f3d25654cc34a
mohamed lotfy <mohhamedlotfy@outlook.com>
2025-08-15 13:11:33 +0300

    Exam

---
+++ OOP/Exam02/FinalExam.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Exam02
+{
+    internal class FinalExam : BaseExam
+    {
+
+        public FinalExam(TimeOnly time, int numberOfQuestions) : base(time, numberOfQuestions)
+        {
+
+        }
+
+        public override void ShowExam()
+        {
+            int totalmarks = 0;
+            int totalPossibleMarks = 0;
+            int questionIndex = 0;
+
+            foreach (var question in Questions)
+            {
+                question.Show();
+                Console.WriteLine($"Question {questionIndex++}: {question.Body}");
+                int ans = int.Parse(Console.ReadLine()!);
+
+                if (question.Answers != null & ans == question.RightAnswers.AnswerId)
+                    totalmarks += int.Parse(question.Mark);
+
+                totalPossibleMarks += int.Parse(question.Mark);
+            }
+            Console.WriteLine($"your Grade :{totalmarks}/{totalPossibleMarks}");
+
+
+        }
+
+
+    }
+}
