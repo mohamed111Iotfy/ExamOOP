d9049f91b039e42ab1173c5419037a1ae09bd84c
mohamed lotfy <mohhamedlotfy@outlook.com>
2025-08-15 13:16:28 +0300

    Exam

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace OOP
+﻿using OOP.Exam02;
+
+namespace OOP
 {
     internal class Program
     {
         static void Main()
         {
-            
+            Subject subject = new Subject(20, "C#");
+            BaseExam exam = new FinalExam(new TimeOnly(3, 0), 50);
+            TrueOrFalse question1 = new TrueOrFalse("Q1", "Is C# a good programming language?", "4");
+            question1.RightAnswers = new Answers(1, "True");
+            exam.Questions[0] = question1;
+            subject.CreateExam(exam);
+            subject.ExamOfSubject.ShowExam();
+
         }
     }
 }
